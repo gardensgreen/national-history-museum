0bd75b981dfe764d01a8da230070bda40df9d9ec
Daniel TIllero <danieltillero67@gmail.com>
2020-11-18 08:40:55 -0500

    fetch list of galleries

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <h1>Hello world!</h1>
-  );
+    const [galleries, setGalleries] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const res = await fetch(
+                    `https://data.nhm.ac.uk/api/3/action/package_list`
+                );
+                if (!res.ok) throw res;
+                const data = await res.json();
+                const { result } = data;
+                setGalleries(result.slice(0, 10));
+            } catch (err) {
+                console.error(err);
+            }
+        })();
+    }, []);
+
+    return <h1>Simple React App</h1>;
 }
 
 export default App;
