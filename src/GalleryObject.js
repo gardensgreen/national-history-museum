33e0e613a2835fa7e5c5579fcb59b2368eef9eb0
Daniel TIllero <danieltillero67@gmail.com>
2020-11-18 09:45:31 -0500

    Flesh out gallery object

--- src/GalleryObject.js
+++ src/GalleryObject.js
@@ -1,5 +1,17 @@
 import React from "react";
 
 export default function GalleryObject(props) {
-    return <a href={`${props.url}`}> {props.name} </a>;
+    return (
+        <>
+            <br />
+            <span>Size: {props.size}</span>
+            <br />
+            <span>format: {props.format}</span>
+            <br />
+            <span>state: {props.state}</span>
+            <br />
+            <a href={`${props.url}`}> {props.name} </a>
+            <br />
+        </>
+    );
 }
